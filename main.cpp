cc8b9813b9709ce6575dcc3a3a03a579a0f7b5a2
amalfouda <109471354+amalfouda@users.noreply.github.com>
2023-10-21 18:11:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,45 +1,65 @@
+//
+//  main.cpp
+//  Digital-project
+//
+//  Created by Amal Fouda on 21/10/2023.
+//
+
 #include <iostream>
 #include <string>
 
 using namespace std;
-
-bool correctOrder(const string& expression)
+bool isValidinput(char c)
 {
-
+    return c>='a' && c<='z';
 }
 
-bool checkSOP(const string& expression) 
+bool checkSOP(const string& expression)
 {
-    if (expression.find('+') != string::npos) //checks if there is a +
-        if (expression.find('(') == string::npos)//and no opening parenthesis
-            return true;
-    return false; 
+    for (char c : expression) {
+            if (c == '+') {
+                if (expression.find('(') == std::string::npos) {
+                    return true; // Found '+' without '('
+                }
+            }
+            else if (!isValidinput(c)) {
+                return false; // Invalid character
+            }
+        }
+        return false;
 }
 
-bool checkPOS(const string& expression) 
+bool checkPOS(const string& expression)
 {
-    if (expression.find('+') == string::npos || expression.find('(') != string::npos) //checks if there's no '+' or if there's an opening parenthesis
-        return expression.find(')') != string::npos;
-    
-    return false; 
+    if (expression.find('+') == std::string::npos || expression.find('(') != std::string::npos) {
+            for (char c : expression) {
+                if (c == ')') {
+                    return true; // Found ')' without '+'
+                }
+                else if (!isValidinput(c)) {
+                    return false; // Invalid character
+                }
+            }
+        }
+        return false;
 }
 
-string check(const string& expression) 
+string check(const string& expression)
 {
-    if (checkSOP(expression)) 
+    if (checkSOP(expression))
         return "SOP";
-    else if (checkPOS(expression)) 
+    else if (checkPOS(expression))
         return "POS";
-    else 
-        return "Neither SOP nor POS";
+    else
+        return "enter valid input";
 }
 
-int main() 
+int main()
 {
     string expression;
     cout << "Enter the boolean function: " << '\n';
-    cin >> expression; 
-    cout << check(expression); 
+    cin >> expression;
+    cout << check(expression);
 
     return 0;
 }
