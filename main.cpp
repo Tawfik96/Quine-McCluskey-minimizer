15dfaa7f2137332ed076cd63f0a7cd890994b731
amalfouda <109471354+amalfouda@users.noreply.github.com>
2023-10-22 16:04:51 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,60 @@
+//
+//  main.cpp
+//  Digital-project
+//
+//  Created by Amal Fouda on 21/10/2023.
+//
+
 #include <iostream>
 #include <string>
 
+
 using namespace std;
-bool isValidinput(char c) //checks that the input is only the alphabet 
+bool isValid(char c)
 {
-    return c>='a' && c<='z';
-}
-
-bool checkSOP(const string& expression)
-{
-    for (char c : expression) {
-            if (c == '+') {
-                if (expression.find('(') == std::string::npos) {
-                    return true; // Found '+' without '('
-                }
-            }
-            else if (!isValidinput(c)) {
-                return false; // Invalid character
-            }
-        }
+    //problem with inverted input
+    if(!(c >= 'a' && c <= 'z') )
         return false;
+    return true;
 }
+bool check_Sop(string str) {
+    bool plus_sign = false;
+    bool last_plussign=false; //to check if there is pus sign at the end
 
+    for (int i=0;i<str.length();i++) {
+        char c=str[i];
+        if (c == '+') {
+            if(i==0 || i==str.length()-1)
+            {
+                last_plussign=true;
+                return false;
+            }
+            plus_sign = true;
+            
+        } else if (c == '(') {
+            return false; // Contains '('
+        } else if (!isValid(c)) {
+            return false; // Invalid character
+        }
+        
+    }
+
+    return plus_sign && !last_plussign;
+}
 bool checkPOS(const string& expression)
 {
+    //if (expression.find('+') == string::npos || expression.find('(') != string::npos) //checks if there's no '+' or if there's an opening parenthesis
+    //    return expression.find(')') != string::npos;
+
+
     int pos = 0;
     int len = expression.length();
 
     while (pos < len) {
-        if (expression[pos] != '(' || !isalpha(expression[pos + 1]))//has to start with a parenthesis followed by a letter 
+        if (expression[pos] != '(' || !isalpha(expression[pos + 1]))//has to start with a parenthesis followed by a letter
             return false;
 
-        pos += 2; // increment to see the character after the ( and the first literal 
+        pos += 2; // increment to see the character after the ( and the first literal
 
         while (pos < len && expression[pos] == '+' && isalpha(expression[pos + 1])) //keep reading while we have "+letter" pattern
             pos += 2; // move past the '+' and the letter
@@ -44,15 +67,17 @@ bool checkPOS(const string& expression)
 
     return true;
 }
-
 string check(const string& expression)
 {
-    if (checkSOP(expression))
+    if (check_Sop(expression))
         return "SOP";
     else if (checkPOS(expression))
         return "POS";
+    
     else
         return "enter valid input";
+
+    
 }
 
 int main()
@@ -61,6 +86,8 @@ int main()
     cout << "Enter the boolean function: " << '\n';
     cin >> expression;
     cout << check(expression);
+   
+
 
     return 0;
 }
