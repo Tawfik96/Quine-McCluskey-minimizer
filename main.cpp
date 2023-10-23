a85aee841716427ba8f93d56406df9704ab5366e
Tawfik96 <eslamtawfik@aucegypt.edu>
2023-10-23 21:55:28 +0300

    Adding Truth Table (SOP)
    adding all the functions and adjustments to display the truth table of the given expression.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-//
-//  main.cpp
-//  Digital-project
-//
-//  Created by Amal Fouda on 21/10/2023.
-//
-
 #include <iostream>
 #include <string>
 #include <map>
+#include<cmath>
+#include<algorithm>
 #include <vector>
 
 
@@ -15,7 +10,7 @@ using namespace std;
 bool isValid(char c)
 {
     //problem with inverted input
-    if(!(c >= 'a' && c <= 'z') )
+    if (!(c >= 'a' && c <= 'z'))
         return false;
     return true;
 }
@@ -32,13 +27,13 @@ bool checkSOP(string str) {
     //             return false;
     //         }
     //         plus_sign = true;
-            
+
     //     } else if (c == '(') {
     //         return false; // Contains '('
     //     } else if (!isValid(c)) {
     //         return false; // Invalid character
     //     }
-        
+
     // }
 
     // return plus_sign && !last_plussign;
@@ -52,11 +47,11 @@ bool checkSOP(string str) {
     {
         char c = str[i];
 
-        if (c == '+') 
+        if (c == '+')
         {
             if (i == 0 || i == str.length() - 1 || last_char_was_plus) //if we are at the beginning or end and we have a +
                 return false; // its invalid 
-            
+
             plus_sign = true; //after checking we dont have + at the beginning or end we change this variable to true 
             last_char_was_plus = true; //and this variable 
             last_char_was_complement = false; // reset after encountering '+'
@@ -65,8 +60,8 @@ bool checkSOP(string str) {
         {
             if (last_char_was_complement || last_char_was_plus)//to check we dont have consecutive inversion after + between literals
                 return false; // its invalid
-           
-            last_char_was_complement = true; 
+
+            last_char_was_complement = true;
             last_char_was_plus = false; // reset after encountering a valid character
         }
         else if (isValid(c)) {
@@ -74,7 +69,7 @@ bool checkSOP(string str) {
             last_char_was_complement = false; // reset after encountering a valid character
         }
         else return false; //its invalid
-        
+
     }
     return plus_sign;
 }
@@ -95,7 +90,7 @@ bool checkPOS(const string& expression)
 
         while (pos < len && expression[pos] == '+' && isalpha(expression[pos + 1])) //keep reading while we have "+letter" pattern
             pos += 2; // move past the '+' and the letter
-        
+
         if (pos >= len || expression[pos] != ')')//has to end in ) after we finish the first ()
             return false;
 
@@ -110,22 +105,20 @@ string check(const string& expression)
         return "SOP";
     else if (checkPOS(expression))
         return "POS";
-    
+
     else
         return "enter valid input";
 
-    
-}
-
-
-
-
-
-
-void separator(string s) {
 
 }
 
+
+
+void display(vector<bool> k) {
+    for (int i = 0; i < k.size(); i++) {
+        cout << k[i] << ",";
+    }
+}
 
 // Function to count how many literals do we have.
 int count_variables(string s) {
@@ -134,66 +127,31 @@ int count_variables(string s) {
     return 0;
 }
 
-//function that recieves values for each literal and the expression 
-// and translate the expression into logic and find its value based on the vlues passed for literals.   {status: Not built yet}
-bool calculate_function(bool values[], string s) {
-
-
-    return values[0] && values[1];
-}
 
 //dec to binary in the proper order abc..  {status: done}
-bool* dec_to_bin(int num, int num_of_bits) {
-    bool* binary_values = new bool[num_of_bits];
-    bool* reversed_binary_values = new bool[num_of_bits];
-
+vector<bool> dec_to_bin(int num, int num_of_bits) {
+    vector<bool>binary_values;
     int temp = 0;
     int i = 0;
     while (num > 0) {
 
         // storing remainder in binary array 
-        binary_values[i] = num % 2;
+        binary_values.push_back(num % 2);
         num = num / 2;
         i++;
     }
     while (i < num_of_bits) {         //continuer the remaining places with zeros
-        binary_values[i] = 0;
+        binary_values.push_back(0);
         i++;
     }
-
     // Inveting the order of the array so that it match the order of the literals.
-    for (int x = 0, j = i - 1; j >= 0; x++, j--)
-        reversed_binary_values[x] = binary_values[j];
-
-    for (int y = 0; y < i; y++) {
-        cout << reversed_binary_values[y];
-    }
-    //cout<<endl;
-
+    reverse(binary_values.begin(), binary_values.end());
     return binary_values;
 }
 
-void truth_table_generator(string s) {
 
-    map<char, int>indx;
-    int num_of_variables = 2;// = count_variables(s)
 
-    bool* values = new bool[num_of_variables];  // array to contain all the temporary values that I will give to the literals
-
-    //creating the indexing map
-    for (int i = 0; i < num_of_variables; i++) {
-        indx[char(97 + i)] = i;
-    }
-    cout << "ab---f" << endl;
-    for (int i = 0; i < 4; i++) {   //i<(2*num_of_varialbes)
-        values = dec_to_bin(i, num_of_variables);
-        cout << "---";
-        cout << calculate_function(values, s) << endl;
-    }
-
-}
-
-vector<string> separator(string s) {
+vector<string> separator_SOP(string s) {
     vector<string> terms;
     string term = "";
     for (int i = 0; i < s.size(); i++)
@@ -208,50 +166,87 @@ vector<string> separator(string s) {
     }
     terms.push_back(term);
 
+
     return terms;
 }
 
-
-bool calculate_expression(bool values[], string term, map<char, int>indx) {    //bcd
+bool calculate_expression_SOP(vector<bool> values, string term, map<char, int>indx) {    //bcd
     bool anding = 1;
     for (int i = 0; i < term.size(); i++) {
-        cout << term[i];
         if (i + 1 < term.size() && term[i + 1] == '\'') {
             anding = (anding && !(values[indx[term[i]]]));
             i++;
         }
         else {
             anding = (anding && values[indx[term[i]]]);
-            cout << anding << endl;
         }
-
     }
     return anding;
 }
 
-
-
-bool calculate_function(bool values[], string s, map<char, int>indx)
+bool calculate_function_SOP(vector<bool> values, string s, map<char, int>indx)
 {
     bool res = 0;
 
     vector<string> terms;
 
-    // if(checkPOS(s)){ //ab+bc
+    if (checkPOS(s)) { //ab+bc
 
-    // }else{ //SoP
+    }
+    else { //SoP
+        terms = separator_SOP(s);
+        for (int i = 0; i < terms.size(); i++) {
+            res = (res || calculate_expression_SOP(values, terms[i], indx));
+        }
+        return res;
 
-    // }
-
-    //assume it is SoP:----
-
-    terms = separator(s);
-    for (int i = 0; i < terms.size(); i++) {
-        res = (res || calculate_expression(values, terms[i], indx));
     }
 
-    return res;
+
 }
+
+
+vector<char> NumberOfLiterals(string str) {
+    vector<char> literals; // Use char instead of string to store individual characters
+
+    for (char c : str) {
+        if (isValid(c)) {
+            literals.push_back(c);
+        }
+    }
+
+    // Sort and remove duplicates
+    sort(literals.begin(), literals.end());
+    literals.erase(unique(literals.begin(), literals.end()), literals.end());
+
+    int count = literals.size();
+    return literals;
+}
+
+void truth_table_generator(string s) {
+
+    map<char, int>indx;
+    vector<char> literals = NumberOfLiterals(s);
+    vector<bool> values;  // vector to contain all the temporary values that I will give to the literals
+    //creating the indexing map
+    for (int i = 0; i < literals.size(); i++) {
+        indx[literals[i]] = i;
+        cout << literals[i] << " | ";
+    }
+    cout << "f" << endl;
+    for (int x = 0; x < literals.size(); x++) { cout << "-----"; }
+    cout << endl;
+
+    for (int i = 0; i < pow(2, literals.size()); i++) {   //i<(2*num_of_varialbes)
+        values = dec_to_bin(i, literals.size());
+        for (int j = 0; j < values.size(); j++) {
+            cout << values[j] << " | ";
+        }
+        cout << calculate_function_SOP(values, s, indx) << endl;
+    }
+
+}
+
 
 
 int countOnes(const string& binaryLiteral) //first step of the implicaton table: to see how many 1's are in each literal 
@@ -268,7 +263,7 @@ int countOnes(const string& binaryLiteral) //first step of the implicaton table:
 
 int findGroup(string s)//second step is to group the literals with the name number of 1's 
 {
-
+    return 0;
 }
 
 //third: we need to check for grey code - adjacent literals - start comparing 
@@ -279,27 +274,15 @@ int findGroup(string s)//second step is to group the literals with the name numb
 
 int main()
 {
-    //string expression;
-    ////cout << "Enter the boolean function: " << '\n';
-    ////cin >> expression;
-    //expression = "ab";
+    string expression;
+    expression = "ab'+ab+c";
     ////cout << check(expression);
 
+    truth_table_generator(expression);
 
-    //truth_table_generator(expression);
-
-    ////bool* binary_values = dec_to_bin(3, 5);
-
-    bool values[3] = { 0,1,1 };
-    string s = "ab+bc+ab'";
-    map<char, int>k;
-    k['a'] = 0;
-    k['b'] = 1;
-    k['c'] = 2;
-
-    cout<<calculate_function(values,s,k);
-    cout << calculate_expression(values, "a'bc", k);
 
 
     return 0;
 }
+
+
